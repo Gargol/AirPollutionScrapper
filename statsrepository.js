50ba6721f390eead0d11eb7cfa115c1dda56d1ca
gargol <nazargargol@gmail.com>
2013-10-25 11:48:36 +0200

    MongoDB repository for data persistance.

---
+++ statsrepository.js
@@ -0,0 +1,36 @@
+var PollutionStats = PollutionStats || {};
+
+var MongoClient = require('mongodb').MongoClient
+  , logger = require('./logger')
+  , Q = require('q');
+
+PollutionStats.StatsRepository = (function () {
+  var
+    client = MongoClient
+    , connectionURL = 'mongodb://localhost/pollution_stats'
+    , insert = function (data, collectionName) {
+      var d = Q.defer();
+
+      client.connect(connectionURL, function (err, db) {
+        if (err) logger.error(err);
+
+        var collection = db.collection(collectionName);
+
+        collection.insert(data, function (err, inserted) {
+          if (err) logger.error(err);
+
+          if (inserted) logger.info('inserted new record into database');
+
+          d.resolve(inserted);
+        });
+      });
+
+      return d.promise;
+    }
+    ;
+  return{
+    insert: insert
+  }
+}());
+
+module.exports = PollutionStats.StatsRepository;
