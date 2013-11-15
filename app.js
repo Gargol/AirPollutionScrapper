12c6f965516b569f02f765670aadfeaab9461d95
gargol <nazargargol@gmail.com>
2013-11-15 16:35:08 +0100

    added proper domain error handling for failed requests.

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var http = require('http')
   , logger = require('./logger')
   , statParser = require('./statisticsParser')
   , configFile = __dirname + '/config.json'
-  , repo = require('./statsrepository');
+  , repo = require('./statsrepository')
+  , domain = require('domain').create();
 
 var configData = fs.readFileSync(configFile, 'utf8');
 
@@ -23,8 +24,13 @@ stations.forEach(function(station){
 
 repo.init({"connectionUrl": config["connectionString"]})
 
-
 var isTaskRunning = false;
+
+domain.on('error', function(err){
+  logger.error(err);
+  isTaskRunning = false;
+});
+
 setInterval(function () {
   if (isTaskRunning) {
     logger.info('Task was not run because previous task has not finished execution');
@@ -32,52 +38,62 @@ setInterval(function () {
   } else {
     isTaskRunning = true;
     logger.info('starting request execution');
-    ProcessAllEndpoints(stations)
-      .then(function () {
-        logger.info('finished request execution');
-        isTaskRunning = false;
-      });
+
+    domain.run(function(){
+      ProcessAllEndpoints(stations)
+        .then(function () {
+          logger.info('finished request execution');
+          isTaskRunning = false;
+        })
+        .fail(function(err){
+          logger.error(err);
+          isTaskRunning = false;
+        });
+    });
   }
-}, requestInterval * 60 * 1000)
+}, requestInterval * 60 * 1000);
 
 
 function ProcessAllEndpoints(endpoints, n, d) {
   n = n || 0;
   d = d || Q.defer();
 
-  if (!endpoints) throw new EventException();
+  if (!endpoints) throw new Error('no endpoints configured');
 
   if (!endpoints[n]) {
     d.resolve();
-
   } else {
     var stationURL = endpoints[n].url;
-    RequestStats(stationURL).then(function (data) {
-      logger.info('processing data from: ' + stationURL);
-      if(data){
-        data["_id"] = endpoints[n].key +'-'+ data.date;
-        data["station_id"] = endpoints[n].key;
+    RequestStats(stationURL)
+      .then(function (data) {
+        logger.info('processing data from: ' + stationURL);
+        if(data){
+          data["_id"] = endpoints[n].key +'-'+ data.date;
+          data["station_id"] = endpoints[n].key;
 
-        repo.get(data["_id"], 'pollution_stats').then(function(repoStat){
-          if(!repoStat){
-            logger.info('inserting new stats: ' );
-            logger.info(data);
-            repo.insert(data, 'pollution_stats')
-              .then(function(){
-                // calling next endpoint whenever previos one is processed
-                ProcessAllEndpoints(endpoints, n + 1, d);
-              });
-          }else{
-            logger.info('received data has already been processed');
-            ProcessAllEndpoints(endpoints, n + 1, d);
-          }
-        });
+          repo.get(data["_id"], 'pollution_stats').then(function(repoStat){
+            if(!repoStat){
+              logger.info('inserting new stats: ' );
+              logger.info(data);
+              repo.insert(data, 'pollution_stats')
+                .then(function(){
+                  // calling next endpoint whenever previous one is processed
+                  ProcessAllEndpoints(endpoints, n + 1, d);
+                });
+            }else{
+              logger.info('received data has already been processed');
+              ProcessAllEndpoints(endpoints, n + 1, d);
+            }
+          });
 
-      }else{
-        logger.error('no data received from endpoint');
-        ProcessAllEndpoints(endpoints, n + 1, d);
-      }
-    });
+        }else{
+          logger.error('no data received from endpoint');
+          ProcessAllEndpoints(endpoints, n + 1, d);
+        }
+      })
+      .fail(function(err){
+        d.reject(err);
+      });
 
     return d.promise;
   }
@@ -88,7 +104,10 @@ function RequestStats(url) {
   var d = Q.defer();
   http.get(url, function (req) {
     req.pipe(bl(function (err, result) {
-      if (err) logger.log(err);
+      if (err){
+        logger.log(err);
+        d.reject(err);
+      }
 
       logger.info('got response from: ' + url);
 
